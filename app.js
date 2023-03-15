46e9fd59b13778f2132155457efb981876475611
Sajal Shakya <73093812+Redstarks@users.noreply.github.com>
2023-03-15 13:56:54 +0545

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,39 @@ app.use(express.json())
 
 app.use('/api', auth_route)
 
+app.use((err,req,res,next) => {
+    console.log(err.message);
+    let status_code = 500;
+    let msg = "Server Error"
+    let errors = [];
+
+    if(err.name === "ValidationError"){
+        status_code = 400;
+        msg = "Bad request"
+        Object.entries(err.errors).map(error => {
+            errors.push({
+                params: error[0],
+                msg:error[0].message
+            })
+        })
+    }else{
+        if(err.code === 11000){
+            status_code = 400;
+            msg = "Bad request"
+            errors.push({
+                params: "email",
+                msg:"duplicate Emails"
+            })
+        }
+    }
+    
+    res.status(status_code).send({
+        msg:msg,
+        errors,
+        error: err.message
+    })
+})
+
 require('dotenv').config()
 require("./config/database")
 app.listen(process.env.PORT, () => {
